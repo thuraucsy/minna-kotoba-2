2490509af6081488c0cbfaaa7e2fe9f52d4dfdd1
Thura Aung <thura.ucsy@gmail.com>
2019-05-08 22:13:28 +0900

    Search function added.

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,6 @@ import 'package:flutter_tts/flutter_tts.dart';
 
 void main() => runApp(MyApp());
 
-Future<List<Chapter>> fetchPhotos(context) async {
-  final response =
-      await DefaultAssetBundle.of(context).loadString('assets/data.json');
-  return compute(parseChapters, response);
-}
-
-List<Chapter> parseChapters(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Chapter>((json) => Chapter.fromJson(json)).toList();
-}
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => new _MyAppState();
@@ -26,6 +15,8 @@ class _MyAppState extends State<MyApp> {
   bool isShowDrawerMenu = true;
   List<Chapter> chapters;
   FlutterTts flutterTts;
+  List<ListItem> allWords = [];
+  List<ListItem> allWordsBackup = [];
   ScrollController _scrollController = new ScrollController();
 
   Future _speak(text) async {
@@ -56,7 +47,8 @@ class _MyAppState extends State<MyApp> {
           });
           print('index $i $drawerIndex');
           Navigator.of(context).pop(); // dismiss the navigator
-          _scrollController.animateTo(0.0, duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+          _scrollController.animateTo(0.0,
+              duration: Duration(milliseconds: 500), curve: Curves.easeOut);
         },
       ));
     }
@@ -89,18 +81,108 @@ class _MyAppState extends State<MyApp> {
         });
   }
 
+  void filterSearchResults(String value) {
+    List<ListItem> searchList = List<ListItem>();
+    searchList.addAll(allWordsBackup);
+    if (value.isNotEmpty) {
+      List<ListItem> searchListFound = List<ListItem>();
+
+      searchList.forEach((item) {
+        if (item is Vocal &&
+            (item.romaji.contains(value) ||
+                item.hiragana.contains(value) ||
+                item.kanji.contains(value) ||
+                item.english.contains(value) ||
+                item.myanmar.contains(value))) {
+          searchListFound.add(item);
+        }
+      });
+
+      setState(() {
+        allWords.clear();
+        allWords.addAll(searchListFound);
+      });
+    } else {
+      setState(() {
+        allWords.clear();
+        allWords.addAll(searchList);
+      });
+    }
+  }
+
+  Container _buildSearchBodyList(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                print('search onChanged $value');
+                filterSearchResults(value);
+              },
+              decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "watashi",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)))),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                controller: _scrollController,
+                itemCount: allWords.length,
+                itemBuilder: (context, index) {
+                  if (allWords[index] is ChapterTitle) {
+                    ChapterTitle chapterTitle = allWords[index] as ChapterTitle;
+                    return ListTile(
+                        title: Text(
+                      chapterTitle.title,
+                      style: Theme.of(context).textTheme.headline,
+                    ));
+                  } else if (allWords[index] is Vocal) {
+                    Vocal vocal = allWords[index] as Vocal;
+                    return ListTile(
+                      title: Text(vocal.hiragana),
+                      subtitle: Text(
+                        vocal.myanmar,
+                        style: TextStyle(fontFamily: 'Masterpiece'),
+                      ),
+                      onTap: () {
+                        _speak(vocal.hiragana);
+                      },
+                      onLongPress: () {},
+                    );
+                  }
+                }),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   void initState() {
-    super.initState();
     print('initState');
+
     fetchPhotos(context).then((data) {
       print('initState data ${data.length}');
       setState(() {
         chapters = data;
       });
+
+      for (int i = 0; i < chapters.length; i++) {
+        allWords.add(ChapterTitle(chapters[i].title));
+        allWords.addAll(chapters[i].words);
+      }
+      print('allWords ${allWords.length}');
+      allWordsBackup.addAll(allWords);
     }).catchError((error) {
       print('initState error $error');
     });
+
+    super.initState();
   }
 
   @override
@@ -115,7 +197,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: appTitle,
       home: DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           appBar: AppBar(title: Text(appTitle)),
           drawer: isShowDrawerMenu
@@ -135,8 +217,9 @@ class _MyAppState extends State<MyApp> {
                   ? _buildBodyList(context, chapters[drawerIndex])
                   : Center(child: CircularProgressIndicator()),
               chapters != null
-                  ? _buildBodyList(context, chapters[drawerIndex])
+                  ? _buildSearchBodyList(context)
                   : Center(child: CircularProgressIndicator()),
+              Icon(Icons.favorite),
               Icon(Icons.settings),
             ],
             physics: NeverScrollableScrollPhysics(),
@@ -145,6 +228,7 @@ class _MyAppState extends State<MyApp> {
             tabs: [
               Tab(icon: Icon(Icons.format_list_numbered_rtl)),
               Tab(icon: Icon(Icons.search)),
+              Tab(icon: Icon(Icons.favorite)),
               Tab(icon: Icon(Icons.settings)),
             ],
             onTap: (int index) {
@@ -168,7 +252,10 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Vocal {
+// The base class for the different types of items the List can contain
+abstract class ListItem {}
+
+class Vocal implements ListItem {
   final String romaji;
   final String hiragana;
   final String kanji;
@@ -188,6 +275,12 @@ class Vocal {
   }
 }
 
+class ChapterTitle implements ListItem {
+  final String title;
+
+  ChapterTitle(this.title);
+}
+
 class Chapter {
   final String title;
   final List<Vocal> words;
@@ -203,4 +296,15 @@ class Chapter {
       words: vocalList,
     );
   }
+}
+
+Future<List<Chapter>> fetchPhotos(context) async {
+  final response =
+      await DefaultAssetBundle.of(context).loadString('assets/data.json');
+  return compute(parseChapters, response);
+}
+
+List<Chapter> parseChapters(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Chapter>((json) => Chapter.fromJson(json)).toList();
 }
